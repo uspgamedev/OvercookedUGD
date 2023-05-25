bf3110cb45d6e20c3de6df61ee142a0f27f50370
GuiHVC <guilherme.cunha1411.mc@gmail.com>
2023-05-24 22:36:07 -0300

    non linear delivery

--- Assets/Scripts/TableTypes/FinalTable.cs
+++ Assets/Scripts/TableTypes/FinalTable.cs
@@ -19,16 +19,32 @@ public class FinalTable : TableClass
     
     public override void UseTable() {
 
-        if(tableTuples[0].ingredient == orderList.GetChild(1).GetComponent<OrderRecipeUI>().thisOrder.dishRecipe){
+        OrderRecipeUI[] orders = orderList.GetComponentsInChildren<OrderRecipeUI>();
+        bool delivered = false;
+        foreach(OrderRecipeUI order in orders){
+            if(tableTuples[0].ingredient == order.thisOrder.dishRecipe){
+                deliveredOrder?.Invoke(this, EventArgs.Empty);
+                order.gameObject.GetComponent<Transform>().DOPunchPosition(new Vector3(-10f, 0f, 0f), 2f);
+                Destroy(order.gameObject);
+                ClearTable();
+                delivered = true;
+                break;
+            }
+        }
+        if(!delivered){
+            ClearTable();
+            Debug.Log("Errado");
+        }
+        /*if(tableTuples[0].ingredient == orderList.GetChild(1).GetComponent<OrderRecipeUI>().thisOrder.dishRecipe){
             deliveredOrder?.Invoke(this, EventArgs.Empty);
             orderList.GetChild(1).gameObject.GetComponent<Transform>().DOPunchPosition(new Vector3(-10f, 0f, 0f), 2f);
-            //Destroy(orderList.GetChild(1).gameObject);
+            Destroy(orderList.GetChild(1).gameObject);
             ClearTable();
         }
         else{
             ClearTable();
             Debug.Log("Errado");
-        }
+        }*/
     }
     
 }
